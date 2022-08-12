d6dfa4a4343abfdec899d58a3bb9dceb379cb50a
minuseno <32548111+minuseno@users.noreply.github.com>
2022-08-12 02:14:30 +0100

    Iterators Added

--- calculations/generic_calculator.js
+++ calculations/generic_calculator.js
@@ -5,7 +5,10 @@ function doOperation(){
     let opr = arguments[0];
     let vals = [...arguments].slice(-args+1);  
     console.log( opr, 'on:', vals );
-    return opr(...vals);
+    return ''+opr(...vals);
+    }
+    else {
+    return ''+arguments[0];
     }
 }
 
@@ -36,3 +39,54 @@ doOperation(function(){
     return ret;
 },-20,'-0.5',0.2);
 
+// universal operations
+// sum
+function sum(){
+    let ret = 0;
+    for (i=0; i<arguments.length; i++) {
+        ret = ret + +arguments[i];
+    }
+    return ret;
+}
+
+// multi
+function multi() {
+    let ret = 1;
+    for (i=0; i<arguments.length; i++) {
+        ret = ret * +arguments[i];
+    }
+    return ret;
+}
+
+// div
+function div(){
+    let ret = 1;
+    for (i=0; i<arguments.length; i++) {
+        ret = ret / +arguments[i];
+    }
+    return ret;
+}
+
+// usages:
+doOperation(multi('2',div('3')));
+
+doOperation(multi(2,div(3)));
+
+doOperation(multi(2,div('3')));
+
+doOperation(multi('2',div(3)));
+
+//'0.6666666666666666' gives String
+
+doOperation(multi,2,doOperation(div,3));
+
+doOperation(multi,'2',doOperation(div,'3'));
+
+doOperation(multi,2,doOperation(div,'3'));
+
+doOperation(multi,'2',doOperation(div,3));
+
+//'0.6666666666666666' gives String
+
+multi('2',div('3'));
+//0.6666666666666666    gives Number
